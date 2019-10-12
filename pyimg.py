a02c9ce2cbcda76c5f6c3e2fdf77d49db142491a
Md Sahil <287mdsahil@gmail.com>
2019-10-12 15:03:06 +0530

    Printed image in terminal

--- pyimg.py
+++ pyimg.py
@@ -7,15 +7,16 @@ import sys
 
 # Loading the image
 def loadImage():
+    maxdim = 300
     img = Image.open(sys.argv[1])
     img.load()
     w = img.size[0]
     h = img.size[1]
     scale = 1
     if(w > h):
-        scale = (100/w)
+        scale = (maxdim/w)
     else:
-        scale = (100/h)
+        scale = (maxdim/h)
     w = int(w*scale)
     h = int(h*scale)
     print(w,h)
@@ -23,7 +24,28 @@ def loadImage():
     img = np.array(img)
     return img
 
+# function to convert rbg color sequence to ansi color code
+def get_ansi_color_code(r, g, b):
+    if r == g and g == b:
+        if r < 8:
+            return 16
+        if r > 248:
+            return 231
+        return round(((r - 8) / 247) * 24) + 232
+    return 16 + (36 * round(r / 255 * 5)) + (6 * round(g / 255 * 5)) + round(b / 255 * 5)
+
+# function to print a pixel
+def printPixel(r,g,b):
+    colorCode = int(get_ansi_color_code(r,g,b))
+    print(("\x1b[48;5;{}m  \x1b[0m".format(int(colorCode))),end="")
+
+#function to print the image
+def printImage(img):
+    for x in img:
+        for y in x:
+            printPixel(y[0],y[1],y[2])
+        print()
 
 ##########################################################
 img = loadImage()
-print(img.shape)
+printImage(img)
